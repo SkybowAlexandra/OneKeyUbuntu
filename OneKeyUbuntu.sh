595ac091e78dbbf2863d4c7f43e6af694ccb8bc7
SkybowAlexandra <1973659295@qq.com>
2024-02-20 17:03:14 +0800

    add http_proxy module

--- OneKeyUbuntu.sh
+++ OneKeyUbuntu.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#代理ip
+http_proxy=http://192.168.1.2:7890
+
+proxy_ip=${http_proxy#http://}  # 移除"http://"
+proxy_ip=${proxy_ip%%:*}        # 截取冒号":"之前的部分，即IP地址
+proxy_port=${http_proxy##*:}    # 截取冒号":"之后的部分，即端口号
+# 输出IP地址和端口
+echo "代理IP地址: $proxy_ip"
+echo "代理端口: $proxy_port"
 
 #要安装的软件包列表
 packages_to_install=(
@@ -20,6 +29,7 @@ packages_to_install=(
 #设置Git用户名和邮箱
 Git_User_Name="SkybowAlexandra"
 Git_User_Email="1973659295@qq.com"
+
 #Git_User_Name=""
 #Git_User_Email=""
 
@@ -65,12 +75,12 @@ function Set_Git_User_And_Email()
 {
     # 检查Git_User_Name是否为空
     if [ -z "$Git_User_Name" ]; then
-        wrn "Git User Name is not set...."
+        wrn "Git用户名未设置...."
         return $EXIT_FAILURE
     fi
     # 检查Git_User_Email是否为空
     if [ -z "$Git_User_Email" ]; then
-        wrn "Git User Email is not set..."
+        wrn "Git用户邮箱未设置..."
         return $EXIT_FAILURE
     fi
     #设置git用户和邮箱
@@ -109,40 +119,64 @@ function Check_Ubuntu_Version()
     fi
 }
 
-
-
-main()
+function Set_Network_Proxy()
 {
+    # 检查是否提供了http_proxy环境变量
+    if [ -z "$http_proxy" ]; then
+        wrn "未设置http代理服务器,可能会出现下载失败"
+        return 1
+    fi
+    # 检查代理地址是否可用
+    curl -x "$http_proxy" -I "www.google.com" --max-time 10 > /dev/null
+    if [ $? -ne 0 ]; then
+        wrn "http代理地址不可用,不使用代理服务器..."
+        return 1
+    fi
 
-    #if [ "$(id -u)" -ne "0" ]; then
-    #    sudo "$0" "$user_dir"     
-    #    exit $EXIT_FAILURE
-    #fi
+    git config --global http.proxy $http_proxy
+    git config --global https.proxy $http_proxy
+    export http_proxy=$http_proxy
+    export https_proxy=$http_proxy
+    log "http代理设置成功..."
+    return 0
+
+}
+
+function Install_Vcpkg()
+{
+    return 0
+}
+
+
+function main()
+{
+    #1.检查系统
     Check_Ubuntu_Version
+    if [ "$?" -eq 0 ]; then
+        log "Ubuntu系统版本检查通过..."
+    else
 
+    fi
+
+    #2.设置网络代理
+    Set_Network_Proxy
+    #3.更新系统
     sudo apt update
     sudo apt upgrade
 
-    if [ "$?" -eq 0 ]; then
-        log "System version check passed..."
-    else
-        err "The ubuntu version must be greater than 22..."
-        exit $EXIT_FAILURE
-    fi
-
-    #2.安装软件包
+    #4.安装软件包
     Instail_Packages
-#
-    #3.设置Git信息
+
+    #5.设置Git信息
     Set_Git_User_And_Email
     if [ "$?" -eq 0 ]; then
-        log "Set Git User Name and Email Success..."
+        log "设置Git用户名和邮箱成功..."
     else
-        wrn "Set Git User Name and Email Failed..."
+        wrn "设置Git用户名和邮箱失败..."
         exit $EXIT_FAILURE
     fi
 
-    #4.创建目录
+    #6.创建目录
     if [ ! -d ~/Softwares ]; then
         mkdir ~/Softwares
     fi
@@ -151,7 +185,7 @@ main()
     fi
     #5.克隆仓库
     cd ~/Softwares
-    git clone $Vcpkg_Repo
+    git clone $Vcpkg_Repo "vcpkg"
 
 
 
